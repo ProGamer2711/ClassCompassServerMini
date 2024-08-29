c59cd79530f486c9689c44f2a4e43f91c1737d8f
Alexander Shestakov <alexandorshestakov@gmail.com>
2024-08-29 14:53:45 +0000

    fixed some type errors

--- routes/index.ts
+++ routes/index.ts
@@ -1,4 +1,4 @@
-import { Request, Response, Router, raw } from "express";
+import { Router, raw } from "express";
 import crypto from "crypto";
 import { exec } from "child_process";
 
@@ -11,43 +11,39 @@ router.get("/", (_, res) => {
 	res.status(200).json({ message: "Сървърът работи нормално." });
 });
 
-router.post(
-	"/webhook",
-	raw({ type: "application/json" }),
-	(req: Request, res: Response) => {
-		const signature = req.headers["x-hub-signature"] as string;
-		if (!signature) {
-			return res.status(400).send("Signature header not found");
-		}
-
-		const payloadBody = JSON.stringify(req.body);
-
-		const hash = crypto
-			.createHmac("sha1", webhookSecret)
-			.update(payloadBody)
-			.digest("hex");
-		const expectedSignature = `sha1=${hash}`;
-
-		if (
-			!crypto.timingSafeEqual(
-				Buffer.from(signature),
-				Buffer.from(expectedSignature)
-			)
-		) {
-			return res.status(403).send("Signature verification failed");
-		}
-
-		// Signature verified, execute git pull
-		exec(`cd ${repoPath} && git pull`, error => {
-			if (error) {
-				console.error("Deployment error:", error);
-				return res.status(500).send("Deployment failed");
-			}
-
-			console.log("Deployment successful");
-			res.status(200).send("Deployment successful");
-		});
+router.post("/webhook", raw({ type: "application/json" }), (req, res) => {
+	const signature = req.headers["x-hub-signature"] as string;
+	if (!signature) {
+		return res.status(400).send("Signature header not found");
 	}
-);
+
+	const payloadBody = JSON.stringify(req.body);
+
+	const hash = crypto
+		.createHmac("sha1", webhookSecret)
+		.update(payloadBody)
+		.digest("hex");
+	const expectedSignature = `sha1=${hash}`;
+
+	if (
+		!crypto.timingSafeEqual(
+			Buffer.from(signature),
+			Buffer.from(expectedSignature)
+		)
+	) {
+		return res.status(403).send("Signature verification failed");
+	}
+
+	// Signature verified, execute git pull
+	exec(`cd ${repoPath} && git pull`, error => {
+		if (error) {
+			console.error("Deployment error:", error);
+			return res.status(500).send("Deployment failed");
+		}
+
+		console.log("Deployment successful");
+		res.status(200).send("Deployment successful");
+	});
+});
 
 export const path = "/";
