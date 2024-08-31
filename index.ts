aebb6739ba459b49556d5ad56a1c51d2fbafba9f
Alexander Shestakov <alexandorshestakov@gmail.com>
2024-08-31 11:11:58 +0000

    added redis connection log

--- index.ts
+++ index.ts
@@ -125,6 +125,10 @@ export const prismaClient = new PrismaClient();
 
 export const redisClient = new Redis(process.env.REDIS_URL ?? "");
 
+redisClient.on("connect", () => {
+	console.log("Connected to Redis");
+});
+
 const port = process.env.PORT ?? 8393;
 
 app.listen(port, () => {
