1304d618ddac563f5f879cd375365469c55d04eb
Alexander Shestakov <alexandorshestakov@gmail.com>
2024-12-22 19:26:49 +0200

    Remove Express files for preparation

--- types/express.d.ts
+++
@@ -1,9 +0,0 @@
-import "express"; // Import Express types for augmentation
-import { Message } from "./messages";
-import { Response as ResponseType } from "./responses";
-
-declare module "express-serve-static-core" {
-	export interface Response {
-		sendResponse: <T>(message: Message, data: T) => void;
-	}
-}
--- types/responses.ts
+++
@@ -1,9 +0,0 @@
-import type { Message } from "./messages";
-
-// Conditional types for success and error responses
-type SuccessResponse<T> = Message & { success: true; data: T };
-
-// TODO: provide better typing for error
-type ErrorResponse = Message & { success: false; error: any };
-
-export type Response<T> = SuccessResponse<T> | ErrorResponse;
